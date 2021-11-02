d7d8998766256bf37d9c6d5fb721b2085697acad
GraciaLazaro <gracia2092@gmail.com>
2021-11-02 15:47:02 +0100

    exercici acabat

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    let tasks = [];
+
+    $('#submit').on("click", function () {
+        let newTask = $('#newTask').val();
+
+        if (newTask !== "") {
+
+            tasks.push(newTask);
+
+            $('#taskList').empty()
+
+            tasks.forEach(t =>
+                $('#taskList').append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+
+    })
+    $('#clearButton').on("click", function () {
+        let newTask = $('#newTask').val();
+
+        if (newTask !== "") {
+
+            tasks.splice(newTask);
+
+            $('#taskList').empty()
+
+             
+        }
+    })
+});
